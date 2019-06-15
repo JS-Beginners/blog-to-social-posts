ea4a0e2cfdf6c879ab8a3bb56a721b45078abf2e
Romeo <romeojeremiah@gmail.com>
2019-06-14 19:52:21 -0500

    modify app.js to add port env statement

--- app.js
+++ app.js
@@ -119,6 +119,6 @@ app.get('/import-guide', function(req, res){
     res.render('import-guide');
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('Server started on port 3000.');
 })
